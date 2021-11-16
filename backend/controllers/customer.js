d0562bd9bf628b33ef814d6929af861903ce233c
jdavidh93 <jossedavidhurtado@hotmail.com>
2021-11-15 22:57:05 -0500

    Se realiza login

--- backend/controllers/customer.js
+++ backend/controllers/customer.js
@@ -1,35 +1,50 @@
 import customer from "../models/customer.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import moment from "moment";
 
 const registerCustomer = async (req, res) => {
   if (!req.body.name || !req.body.email || !req.body.password)
-    return res.status(400).send("Incomplete data");
+    return res.status(400).send({ message: "Incomplete data" });
 
   const existinCustomer = await customer.findOne({ name: req.body.name });
   if (existinCustomer)
-    return res.status(400).send("The customer already exist");
+    return res.status(400).send({ message: "The customer already exist" });
+
+  const hash = await bcrypt.hash(req.body.password, 10);
 
   const customerSchema = new customer({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hash,
     dbStatus: true,
   });
 
   const result = await customerSchema.save();
-  if (!result) return res.status(400).send("Failed to register customer");
+  if (!result)
+    return res.status(400).send({ message: "Failed to register customer" });
   return res.status(200).send({ result });
 };
 
 const listCustomer = async (req, res) => {
   const customerSchema = await customer.find();
   if (!customerSchema || listCustomer.length == 0)
-    return res.status(400).send("Empty customer list");
+    return res.status(400).send({ message: "Empty customer list" });
   return res.status(200).send({ customerSchema });
 };
 
 const updateCustomer = async (req, res) => {
   if (!req.body.name || !req.body.email || !req.body.password)
-    return res.status(400).send("Incomplete data");
+    return res.status(400).send({ message: "Incomplete data" });
+
+  let pass = "";
+
+  if (req.body.password) {
+    pass = await bcrypt.hash(req.body.password, 10);
+  } else {
+    const customerFind = await user.findOne({ email: req.body.email });
+    pass = customerFind.password;
+  }
 
   const existinCustomer = await customer.findOne({
     name: req.body.name,
@@ -37,14 +52,15 @@ const updateCustomer = async (req, res) => {
     password: req.body.password,
   });
   if (existinCustomer)
-    return res.status(400).send("The customer already exist");
+    return res.status(400).send({ message: "The customer already exist" });
 
   const customerUpdate = await customer.findByIdAndUpdate(req.body._id, {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
-  if (!customerUpdate) return res.status(400).send("Error editing customer");
+  if (!customerUpdate)
+    return res.status(400).send({ message: "Error editing customer" });
   return res.status(200).send({ customerUpdate });
 };
 
@@ -53,8 +69,37 @@ const deleteCustomer = async (req, res) => {
     _id: req.params["_id"],
   });
 
-  if (!customerDelete) res.status(400).send("Customer no found");
-  return res.status(200).send("Customer deleted");
+  if (!customerDelete) res.status(400).send({ message: "Customer no found" });
+  return res.status(200).send({ message: "Customer deleted" });
+};
+
+const login = async (req, res) => {
+  if (!req.body.email || !req.body.password)
+    return res.status(400).send({ message: "Incomplete data" });
+
+  const userLogin = await user.findOne({ email: req.body.email });
+  if (!userLogin)
+    return res.status(400).send({ message: "Wrong email or password" });
+
+  const hash = await bcrypt.compare(req.body.password, userLogin.password);
+  if (!hash)
+    return res.status(400).send({ message: "Wrong email or password" });
+
+  try {
+    return res.status(200).json({
+      token: jwt.sign(
+        {
+          _id: userLogin._id,
+          email: userLogin.email,
+          password: userLogin.password,
+          iat: moment().unix(),
+        },
+        process.env.SECRE_KEY_JWT
+      ),
+    });
+  } catch (e) {
+    return res.status(400).send({ message: "Login error" });
+  }
 };
 
 export default {
@@ -62,4 +107,5 @@ export default {
   listCustomer,
   updateCustomer,
   deleteCustomer,
+  login,
 };
--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -3,6 +3,7 @@ import customer from "../controllers/customer.js";
 const router = express.Router()
 
 router.post("/registerCustomer", customer.registerCustomer);
+router.post("/login", customer.login);
 router.get("/listCustomer", customer.listCustomer);
 router.put("/updateCustomer", customer.updateCustomer);
 router.delete("/deleteCustomer/:_id", customer.deleteCustomer);
